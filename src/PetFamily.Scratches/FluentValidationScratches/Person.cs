02ccf9212a202b46f4674bb371b659c55dd62d39
Не Скажу <jimkrauz@gmail.com>
2025-01-28 02:37:20 +0700

    added scratches console project

---
+++ src/PetFamily.Scratches/FluentValidationScratches/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace PetFamily.Scratches.FluentValidationScratches;
+
+public class Person(PersonName name, Contacts contacts)
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public PersonName Name { get; private set; } = name;
+    public Contacts Contacts { get; private set; } = contacts;
+}
---
+++ src/PetFamily.Scratches/FluentValidationScratches/PersonName.cs
@@ -0,0 +1,22 @@
+﻿using PetFamily.Scratches.Common.ResultPattern;
+
+namespace PetFamily.Scratches.FluentValidationScratches;
+
+public sealed record PersonName
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string Patronymic { get; }
+
+    private PersonName(string firstName, string lastName, string patronymic) =>
+        (FirstName, LastName, Patronymic) = (firstName, lastName, patronymic);
+
+    public static Result<PersonName> Create(string? firstName, string? lastName, string? patronymic)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            return new Error("First name is required", ErrorCodes.SomeCode);
+        if (string.IsNullOrWhiteSpace(lastName))
+            return new Error("Last name is required", ErrorCodes.SomeCode);
+        return new PersonName(firstName, lastName, string.IsNullOrWhiteSpace(patronymic) ? "" : patronymic);
+    }
+}
